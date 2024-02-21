ac973d43079ac51c927c88c21c2614e9cbc4123d
Arthur BRady <bradyarthur@users.noreply.github.com>
2024-02-21 10:11:58 -0500

    move

--- cyTest/cypress/e2e/py.spec.cy.js
+++ cyTest/cypress/e2e/py.spec.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8000/')
+
+    cy.get('[data-testid="about_nav"]')
+      .click();
   })
 })
--- test/bak/test_content.py
+++ test/bak/test_content.py
@@ -2,7 +2,7 @@ import unittest
 import shutil
 import os
 
-import makesite
+# import makesite
 from test import path
 
 
