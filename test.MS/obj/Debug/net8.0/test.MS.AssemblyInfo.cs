44ae9fe8bfa55e602807c659a67cba8f0c46f9d2
Arthur BRady <bradyarthur@users.noreply.github.com>
2024-02-26 11:28:46 -0500

    ..

--- test.MS/obj/Debug/net8.0/test.MS.AssemblyInfo.cs
+++ test.MS/obj/Debug/net8.0/test.MS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test.MS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b144c74cbbc77e7b82c8738a1bfa9e5c3ff7f19b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4188e9c07c296ea4314f0324ef87ba1d28307885")]
 [assembly: System.Reflection.AssemblyProductAttribute("test.MS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test.MS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
