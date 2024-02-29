f767701275181af9695df1695e35b4f2a9922cb1
Arthur BRady <bradyarthur@users.noreply.github.com>
2024-02-29 10:23:50 -0500

    .

---
+++ test.MS/test.NS copy.cs
@@ -0,0 +1,15 @@
+namespace nameSpace.Test
+{
+    namespace nameSpace.Test
+    {
+        namespace nameSpace.Test
+        {
+            public class ABX {
+                console.writeline('ABX')
+            }
+            // Classes, interfaces, etc. go here
+        }
+        // Classes, interfaces, etc. go here
+    }
+    // Classes, interfaces, etc. go here
+}
